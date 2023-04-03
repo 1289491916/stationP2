ff8dc267fe7591141b0296ab40c772549e153149
gofee199 <129756726+gofee199@users.noreply.github.com>
2023-04-03 21:04:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
- https://github.com/istoreos/istoreos
+# https://github.com/istoreos/istoreos
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
